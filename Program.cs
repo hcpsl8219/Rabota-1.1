7891a1ac033e687b353ea0fa77a26ad3f19ed49d
hcpsl8219 <96689234+hcpsl8219@users.noreply.github.com>
2021-12-26 17:53:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rabota_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Math.Pow(s, 2) + Math.Pow(s, -2) / Math.Pow(s, 2) - Math.Pow(s, -2));
+            Console.ReadKey();
+        }
+
+        public static double Th(double x)
+        {
+            const double eps = 1e-15;  // погрешность вычислений
+            double s = 0;  // начальная сумма
+            double r = x;   // первый член ряда
+            int n = 0;    // показатель степени
+            while (Math.Abs(r) > eps) // условие выполнения цикла
+            {
+                s = s + r;      // добавление члена ряда
+                n = n + 2;  // наращивание  n:  2,4,6,8,...
+                r = -r * Math.Pow(s, 2) + Math.Pow(s, -2) / Math.Pow(s, 2) - Math.Pow(s, -2); // новый член ряда
+            }
+            return s;       // возврат результата
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Math.Round(Math.Tanh(Math.Pow(s, 2) + Math.Pow(s, -2) / Math.Pow(s, 2) - Math.Pow(s, -2)),
+                Math.Round(ConsoleApp2.Program.Th(Math.Pow(s, 2) + Math.Pow(s, -2) / Math.Pow(s, 2) - Math.Pow(s, -2));
+        }
+    }
+}
